564f56f9bfcc92f21a725eef7b980dbb54b814c3
ty4apex <ty4apex@users.noreply.github.com>
2026-03-12 20:16:11 +0900

    一部修正

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-const CACHE_NAME = 'formcompare-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'formcompare-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.png', '/icon512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
